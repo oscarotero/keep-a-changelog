246452726e3c2f51d3f6bccd4c26159983c48681
Ted Janeczko <tjaneczko@brightcove.com>
2018-08-21 21:20:32 -0400

    Fix trailing newlines when no links are present

--- test/index.js
+++ test/index.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
-const { parser } = require('../src');
+const { parser, Changelog } = require('../src');
 const assert = require('assert');
 
 const changelog = parser(fs.readFileSync(__dirname + '/changelog.md', 'UTF-8'));
 const expected = fs.readFileSync(__dirname + '/changelog.expected.md', 'UTF-8');
+const emptyExpected = fs.readFileSync(__dirname + '/empty.expected.md', 'UTF-8');
 
 //fs.writeFileSync(__dirname + '/changelog.expected.md', changelog.toString());
 // console.log(changelog.toString());
 
 describe('Changelog testing', function() {
+    it('should generate an empty changelog', function() {
+        assert.equal(new Changelog('Changelog').toString(), emptyExpected);
+    });
+
     it('should match the generated changelog with the expected', function() {
         assert.equal(changelog.toString().trim(), expected.trim());
     });
