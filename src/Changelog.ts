0b7b24d5d02100dc4a0e25b890831f579653a4ba
Yannik Beaulieu <yannik.beaulieu2@ia.ca>
2025-02-04 20:29:56 +0000

    Add Azure DevOps URL support

--- src/Changelog.ts
+++ src/Changelog.ts
@@ -13,7 +13,7 @@ export default class Changelog {
   tagNameBuilder?: (release: Release) => string;
   /** @deprecated: Use tagLinkBuilder() instead */
   compareLinkBuilder?: (previous: Release, release: Release) => string;
-  tagLinkBuilder?: (url: string, tag: string, previous?: string) => string;
+  tagLinkBuilder?: (url: string, tag: string, previous?: string, head?: string) => string;
   format: "compact" | "markdownlint" = "compact";
 
   constructor(title: string, description = "") {
@@ -53,17 +53,18 @@ export default class Changelog {
       const url = this.url!;
 
       if (!previous) {
-        return this.tagLinkBuilder(this.url!, this.tagName(release));
+        return this.tagLinkBuilder(this.url!, this.tagName(release), undefined, this.head);
       }
 
       if (!release.date || !release.version) {
-        return this.tagLinkBuilder(url, this.head, this.tagName(previous));
+        return this.tagLinkBuilder(url, this.head, this.tagName(previous), this.head);
       }
 
       return this.tagLinkBuilder(
         url,
         this.tagName(release),
         this.tagName(previous),
+        this.head,
       );
     }
 
