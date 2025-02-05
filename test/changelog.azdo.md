2e8e3416e2b6ec0be049acbc48ed85577ea88264
Yannik Beaulieu <yannik.beaulieu2@ia.ca>
2025-02-05 13:54:46 -0500

    Add unit test

---
+++ test/changelog.azdo.md
@@ -0,0 +1,24 @@
+# Changelog - Azure DevOps demo
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](https://semver.org/).
+
+## [Unreleased]
+- This is a brand new releases with:
+
+## [0.0.2] - 2023-11-09
+### Added
+- I want to have a compare link
+
+## [0.0.1] - 2023-11-09
+### Added
+- This is just a test changelog
+
+[Unreleased]: https://dev.azure.com/myorg/myproject/_git/myrepo/branchCompare?baseVersion=GTv0.0.2&targetVersion=GBmaster
+[0.0.2]: https://dev.azure.com/myorg/myproject/_git/myrepo/branchCompare?baseVersion=GTv0.0.1&targetVersion=GTv0.0.2
+[0.0.1]: https://dev.azure.com/myorg/myproject/_git/myrepo?version=GTv0.0.1
+
+---
+
+This is a footer
--- test/parser.test.ts
+++ test/parser.test.ts
@@ -5,8 +5,10 @@ import getSettingsForURL from "../src/settings.ts";
 
 const file = new URL("./changelog.custom.type.md", import.meta.url).pathname;
 const fileGitlab = new URL("./changelog.gitlab.md", import.meta.url).pathname;
+const fileAzdo = new URL("./changelog.azdo.md", import.meta.url).pathname;
 const changelogContent = Deno.readTextFileSync(file);
 const changelogContentGitlab = Deno.readTextFileSync(fileGitlab);
+const changelogContentAzdo = Deno.readTextFileSync(fileAzdo);
 
 Deno.test("parser testing", function () {
   // is unable to parse changelog with unknown types
@@ -36,4 +38,24 @@ Deno.test("parser testing gitlab", function () {
   }
 
   assertEquals(changelog.toString().trim(), changelogContentGitlab.trim());
+});
+
+Deno.test("parser testing Azure DevOps", function () {
+  // parses a changelog with Azure DevOps links
+  const changelog = parser(changelogContentAzdo, );
+
+  // get settings from url
+  assert(changelog.url, "URL is not defined");
+
+  if(changelog.url) {
+    const settings = getSettingsForURL(changelog.url);
+    assert(settings)
+
+    if (settings) {
+      changelog.head = settings.head;
+      changelog.tagLinkBuilder = settings.tagLink;
+    }
+  }
+
+  assertEquals(changelog.toString().trim(), changelogContentAzdo.trim());
 });
