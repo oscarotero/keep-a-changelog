f235f7018e1806377700a638610fb9d94e31ef38
Reynaldo Gil <gilcu2@gmail.com>
2025-03-23 14:07:23 +0100

    added latest-release-full command

--- bin.ts
+++ bin.ts
@@ -1,220 +1,233 @@
-import { join } from "jsr:@std/path@1.0.8";
-import { Changelog, parser, Release } from "./mod.ts";
-import { parseArgs } from "jsr:@std/cli@1.0.13/parse-args";
-import { parse as parseIni } from "jsr:@std/ini@0.225.2";
+import {join} from "jsr:@std/path@1.0.8";
+import {Changelog, parser, Release} from "./mod.ts";
+import {parseArgs} from "jsr:@std/cli@1.0.13/parse-args";
+import {parse as parseIni} from "jsr:@std/ini@0.225.2";
 import getSettingsForURL from "./src/settings.ts";
 
 const argv = parseArgs(Deno.args, {
-  default: {
-    file: "CHANGELOG.md",
-    format: "compact",
-    release: null,
-    create: null,
-    url: null,
-    https: true,
-    quiet: false,
-    head: null,
-    combine: false,
-    "bullet-style": "-",
-  },
-  string: ["file", "format", "url", "head", "bullet-style"],
-  boolean: [
-    "https",
-    "init",
-    "latest-release",
-    "quiet",
-    "help",
-    "combine",
-    "no-v-prefix",
-    "no-sort-releases",
-  ],
-  alias: {
-    h: "help",
-  },
+    default: {
+        file: "CHANGELOG.md",
+        format: "compact",
+        release: null,
+        create: null,
+        url: null,
+        https: true,
+        quiet: false,
+        head: null,
+        combine: false,
+        "bullet-style": "-",
+    },
+    string: ["file", "format", "url", "head", "bullet-style"],
+    boolean: [
+        "https",
+        "init",
+        "latest-release",
+        "latest-release-full",
+        "quiet",
+        "help",
+        "combine",
+        "no-v-prefix",
+        "no-sort-releases",
+    ],
+    alias: {
+        h: "help",
+    },
 });
 
 const file = join(Deno.cwd(), argv.file);
 
 try {
-  if (argv.help) {
-    showHelp();
-    Deno.exit(0);
-  }
+    if (argv.help) {
+        showHelp();
+        Deno.exit(0);
+    }
 
-  if (argv.init) {
-    const changelog = new Changelog("Changelog").addRelease(
-      new Release("0.1.0", new Date(), "First version"),
-    );
+    if (argv.init) {
+        const changelog = new Changelog("Changelog").addRelease(
+            new Release("0.1.0", new Date(), "First version"),
+        );
 
+        changelog.format = argv.format as "compact" | "markdownlint";
+        changelog.bulletStyle = argv["bullet-style"] as "-" | "*" | "+";
+
+        save(file, changelog, true);
+        Deno.exit(0);
+    }
+
+    const changelog = parser(Deno.readTextFileSync(file), {
+        autoSortReleases: !argv["no-sort-releases"],
+    });
     changelog.format = argv.format as "compact" | "markdownlint";
     changelog.bulletStyle = argv["bullet-style"] as "-" | "*" | "+";
-
-    save(file, changelog, true);
-    Deno.exit(0);
-  }
-
-  const changelog = parser(Deno.readTextFileSync(file), {
-    autoSortReleases: !argv["no-sort-releases"],
-  });
-  changelog.format = argv.format as "compact" | "markdownlint";
-  changelog.bulletStyle = argv["bullet-style"] as "-" | "*" | "+";
-  if (argv["no-v-prefix"]) {
-    changelog.tagNameBuilder = (release) => String(release.version);
-  }
-
-  if (argv["latest-release"]) {
-    const release = changelog.releases.find((release) =>
-      release.date && release.version
-    );
-
-    if (release) {
-      console.log(release.version?.toString());
+    if (argv["no-v-prefix"]) {
+        changelog.tagNameBuilder = (release) => String(release.version);
     }
 
-    Deno.exit(0);
-  }
+    if (argv["latest-release"]) {
+        const release = changelog.releases.find((release) =>
+            release.date && release.version
+        );
 
-  if (argv.release) {
-    const release = changelog.releases.find((release) => {
-      if (release.date) {
-        return false;
-      }
-
-      if (typeof argv.release === "string") {
-        return !release.version || argv.release === release.version.toString();
-      }
-
-      return !!release.version;
-    });
-
-    if (release) {
-      release.date = new Date();
-      if (typeof argv.release === "string") {
-        release.setVersion(argv.release);
-      }
-    } else {
-      console.error("Not found any valid unreleased version");
-      Deno.exit(1);
-    }
-  }
-
-  if (argv.combine) {
-    const combinedReleases = changelog.releases.reduce((acc, release) => {
-      if (release.version) {
-        if (acc[release.version]) {
-          acc[release.version].combineChanges(release.changes);
-        } else {
-          acc[release.version] = release;
+        if (release) {
+            console.log(release.version?.toString());
         }
-      }
-      return acc;
-    }, {} as Record<string, typeof changelog.releases[0]>);
 
-    changelog.releases = Object.values(combinedReleases);
-  }
-
-  if (argv.create) {
-    const version = typeof argv.create === "string" ? argv.create : undefined;
-
-    const release = changelog.releases.find((release) => {
-      return release.version === version;
-    });
-
-    if (release) {
-      console.warn("Release already exists.");
-    } else {
-      changelog.addRelease(new Release(version));
+        Deno.exit(0);
     }
-  }
 
-  save(file, changelog);
+    if (argv["latest-release-full"]) {
+        const release = changelog.releases.find((release) =>
+            release.date && release.version
+        );
+
+        if (release) {
+            console.log(release.toString());
+        }
+
+        Deno.exit(0);
+    }
+
+    if (argv.release) {
+        const release = changelog.releases.find((release) => {
+            if (release.date) {
+                return false;
+            }
+
+            if (typeof argv.release === "string") {
+                return !release.version || argv.release === release.version.toString();
+            }
+
+            return !!release.version;
+        });
+
+        if (release) {
+            release.date = new Date();
+            if (typeof argv.release === "string") {
+                release.setVersion(argv.release);
+            }
+        } else {
+            console.error("Not found any valid unreleased version");
+            Deno.exit(1);
+        }
+    }
+
+    if (argv.combine) {
+        const combinedReleases = changelog.releases.reduce((acc, release) => {
+            if (release.version) {
+                if (acc[release.version]) {
+                    acc[release.version].combineChanges(release.changes);
+                } else {
+                    acc[release.version] = release;
+                }
+            }
+            return acc;
+        }, {} as Record<string, typeof changelog.releases[0]>);
+
+        changelog.releases = Object.values(combinedReleases);
+    }
+
+    if (argv.create) {
+        const version = typeof argv.create === "string" ? argv.create : undefined;
+
+        const release = changelog.releases.find((release) => {
+            return release.version === version;
+        });
+
+        if (release) {
+            console.warn("Release already exists.");
+        } else {
+            changelog.addRelease(new Release(version));
+        }
+    }
+
+    save(file, changelog);
 } catch (err) {
-  console.error(red((err as Error).message));
+    console.error(red((err as Error).message));
 
-  if (!argv.quiet) {
-    Deno.exit(1);
-  }
+    if (!argv.quiet) {
+        Deno.exit(1);
+    }
 }
 
 function save(file: string, changelog: Changelog, isNew = false) {
-  changelog.url = argv.url || changelog.url || getRemoteUrl(argv.https);
+    changelog.url = argv.url || changelog.url || getRemoteUrl(argv.https);
 
-  if (!changelog.url) {
-    console.error(
-      red(
-        'Please, set the repository url with --url="https://github.com/username/repository"',
-      ),
-    );
-    changelog.url = "https://example.com";
-  }
-
-  if (changelog.url) {
-    const settings = getSettingsForURL(changelog.url);
-
-    if (settings) {
-      changelog.head = settings.head;
-      changelog.tagLinkBuilder = settings.tagLink;
+    if (!changelog.url) {
+        console.error(
+            red(
+                'Please, set the repository url with --url="https://github.com/username/repository"',
+            ),
+        );
+        changelog.url = "https://example.com";
     }
-  }
 
-  if (argv.head) {
-    changelog.head = argv.head;
-  }
+    if (changelog.url) {
+        const settings = getSettingsForURL(changelog.url);
 
-  Deno.writeTextFileSync(file, changelog.toString());
+        if (settings) {
+            changelog.head = settings.head;
+            changelog.tagLinkBuilder = settings.tagLink;
+        }
+    }
 
-  if (isNew) {
-    console.log(green("Generated new file"), file);
-  } else {
-    console.log(green("Updated file"), file);
-  }
+    if (argv.head) {
+        changelog.head = argv.head;
+    }
+
+    Deno.writeTextFileSync(file, changelog.toString());
+
+    if (isNew) {
+        console.log(green("Generated new file"), file);
+    } else {
+        console.log(green("Updated file"), file);
+    }
 }
 
 function red(message: string) {
-  return "\u001b[" + 31 + "m" + message + "\u001b[" + 39 + "m";
+    return "\u001b[" + 31 + "m" + message + "\u001b[" + 39 + "m";
 }
 
 function green(message: string) {
-  return "\u001b[" + 32 + "m" + message + "\u001b[" + 39 + "m";
+    return "\u001b[" + 32 + "m" + message + "\u001b[" + 39 + "m";
 }
 
 function normalizeUrl(url: string, https: boolean) {
-  // remove .git suffix
-  url = url.replace(/\.git$/, "");
+    // remove .git suffix
+    url = url.replace(/\.git$/, "");
 
-  // normalize git@host urls
-  if (url.startsWith("git@")) {
-    url = url.replace(
-      /^git@([^:]+):(.*)$/,
-      (https ? "https" : "http") + "://$1/$2",
-    );
-  }
+    // normalize git@host urls
+    if (url.startsWith("git@")) {
+        url = url.replace(
+            /^git@([^:]+):(.*)$/,
+            (https ? "https" : "http") + "://$1/$2",
+        );
+    }
 
-  // remove trailing slashes
-  url = url.replace(/\/+$/, "");
-  return new URL(url);
+    // remove trailing slashes
+    url = url.replace(/\/+$/, "");
+    return new URL(url);
 }
 
 function getRemoteUrl(https = true) {
-  try {
-    const file = join(Deno.cwd(), ".git", "config");
-    const content = Deno.readTextFileSync(file);
-    const data = parseIni(content);
-    const origin = data['remote "origin"'] as { url?: string };
+    try {
+        const file = join(Deno.cwd(), ".git", "config");
+        const content = Deno.readTextFileSync(file);
+        const data = parseIni(content);
+        const origin = data['remote "origin"'] as { url?: string };
 
-    if (!origin?.url) {
-      return;
+        if (!origin?.url) {
+            return;
+        }
+
+        return normalizeUrl(origin.url, https).href;
+    } catch (err) {
+        console.error(red((err as Error).message));
+        // Ignore
     }
-
-    return normalizeUrl(origin.url, https).href;
-  } catch (err) {
-    console.error(red((err as Error).message));
-    // Ignore
-  }
 }
 
 function showHelp() {
-  console.log(`keep-a-changelog
+    console.log(`keep-a-changelog
 
 Usage: keep-a-changelog [options]
 
@@ -226,6 +239,7 @@ Options:
 
   --init              Initialize a new changelog file
   --latest-release    Print the latest release version
+  --get               Print release body
 
   --release           Set the date of the specified release
   --combine           Combine changes from releases with the same version
